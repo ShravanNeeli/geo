af9424083ecb1f664f754124f4299189fbb2d399
shravan <shravanneeli2001@gmail.com>
2024-09-04 11:14:37 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,19 @@
+// export const environment = {
+//     production: true,
+//     // apiUrl: 'https://api.example.com',
+//     featureFlag: true
+//   };
 export const environment = {
-    production: true,
-    // apiUrl: 'https://api.example.com',
-    featureFlag: true
-  };
-  
+  production: true,
+  featureFlag: true,
+  firebase: {
+    apiKey: 'AIzaSyBVK2Ut5IBUa02v9gkg4frZHju-XIAJxE0',
+    authDomain: 'angulargeo-47916.firebaseapp.com',
+    databaseURL: 'https://angulargeo-47916-default-rtdb.firebaseio.com',
+    projectId: 'angulargeo-47916',
+    storageBucket: 'angulargeo-47916.appspot.com',
+    messagingSenderId: '515668449417',
+    appId: '1:515668449417:web:ee3a7dbf708149de374e58',
+    measurementId: 'G-GL70YTZNQC'
+  }
+};
